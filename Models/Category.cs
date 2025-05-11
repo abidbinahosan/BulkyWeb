dec6f73c7e9a25a27a1ed1346da3f73b43bcf42e
abid-bin-ahosan <abid.ahosan@brainstation-23.com>
2025-05-12 00:49:05 +0600

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } //Primary Key
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage = "Can't add more that 30 characters as name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Range of display order must be in range 1-100")]
+        public int DisplayOrder { get; set; }
+    }
+}
