eea591029e719f57f2a8aac158f156a32469fcdb
abid-bin-ahosan <abid.ahosan@brainstation-23.com>
2025-05-12 11:28:28 +0600

    Resolved merge conflicts

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    //Basic Configuration of Entity Framework
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) // Default method in the DbContest (EF core)
+        {
+            modelBuilder.Entity<Category>().HasKey(c => c.Id); // Primary key
+
+            modelBuilder.Entity<Category>().HasData( // Seed data
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "Comedy", DisplayOrder = 3 },
+                new Category { Id = 4, Name = "Drama", DisplayOrder = 4 }
+            );
+        }
+    }
+}
---
+++ BulkyWeb/Migrations/20250417020354_SeedCategoryTable.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Categories",
+                newName: "Id");
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "Action" },
+                    { 2, 2, "SciFi" },
+                    { 3, 3, "Comedy" },
+                    { 4, 4, "Drama" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Categories",
+                newName: "ID");
+        }
+    }
+}
