fb5150e4429e88ed79a9d30d896fe2880721692c
abid-bin-ahosan <abid.ahosan@brainstation-23.com>
2025-05-14 09:04:15 +0600

    Made the project as N-Tier Architechutre

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,7 +1,7 @@
-﻿using BulkyWeb.Models;
+﻿using Bulky.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyWeb.Data
+namespace Bulky.DataAccess.Data
 {
     //Basic Configuration of Entity Framework
     public class ApplicationDbContext : DbContext
